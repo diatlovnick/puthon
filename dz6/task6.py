ebfd0f6c2b1b30f57b28b8831d97c84882041f1f
Nickolai <datlovnick@mail.ru>
2022-08-02 18:37:34 +0300

    5,6 task

--- dz6/task6.py
+++ dz6/task6.py
@@ -1,2 +1,8 @@
 # 6-Сформировать список из  N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
+
+n = 5
+def get_sequence(n):
+    return [-3 **i for i in range(1, n+1)]
+res = get_sequence(n)
+print(res)
